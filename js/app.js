54765a5441cf2abb58af54f751adda133ecc1b1d
Sean Wilson <seandionewilson@gmail.com>
2018-10-28 10:08:12 -0700

    app.js unnecessary comments deleted

--- js/app.js
+++ js/app.js
@@ -1,26 +1,18 @@
-//init allEnemies Array
-//for each enemy create and push new enemy object into above array
 
 
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     //x position
     //y position
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+
+      this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
     //if enemy is not passed boundary
         //move forward
@@ -34,14 +26,11 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//new hero object
 
+//init allEnemies Array
+//for each enemy create and push new enemy object into above array
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 //Hero class
   //Constructor
@@ -77,8 +66,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-//new hero objects
-
-//init allEnemies Array
-//for each enemy create and push new enemy object into above array
